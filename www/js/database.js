f1aefe17fbe1e8f5a9744bfde697e1b0dadbceb0
chilichilimo <afshar.m.95@gmail.com>
2016-03-22 15:10:12 +0000

    Project whole changes

--- www/js/database.js
+++
@@ -1,9 +0,0 @@
-/*
- * Please see the included README.md file for license terms and conditions.
- */
-
- //This file is to deal with user data in the database.
- //This file handles the logins and signups
- //
-
-var user = new Firebase("URL");
