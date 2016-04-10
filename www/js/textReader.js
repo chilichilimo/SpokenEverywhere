0eb376417ffc79443637e74e75b208f9675a52dd
chilichilimo <afshar.m.95@gmail.com>
2016-04-10 11:21:03 +0100

    Latest changes

--- www/js/textReader.js
+++ www/js/textReader.js
@@ -6,65 +6,41 @@
 //The Virtual bookmarking is done here.
 //Reading out is done here.
 
-// function sound(source,volume,loop)
-// {
-//     this.source=source;
-//     this.volume=volume;
-//     this.loop=loop;
-//     var son;
-//     this.son=son;
-//     this.finish=false;
-//     this.stop=function()
-//     {
-//         document.body.removeChild(this.son);
-//     }
-//     this.start=function()
-//     {
-//         if(this.finish)return false;
-//         this.son=document.createElement("embed");
-//         this.son.setAttribute("src",this.source);
-//         this.son.setAttribute("hidden","true");
-//         this.son.setAttribute("volume",this.volume);
-//         this.son.setAttribute("autostart","true");
-//         this.son.setAttribute("loop",this.loop);
-//         document.body.appendChild(this.son);
-//     }
-//     this.remove=function()
-//     {
-//         document.body.removeChild(this.son);
-//         this.finish=true;
-//     }
-//     this.init=function(volume,loop)
-//     {
-//         this.finish=false;
-//         this.volume=volume;
-//         this.loop=loop;
-//     }
-// }
-
-// function play()
-// {
-//   i = bookmark;
-//   while (true)
-//   {
-//     ivona.createVoice(mainTextArray[i]).pipe(fs.createWriteStream('../sound/test.mp3'));
-//     sound('../sound/test.mp3',80,false);
-//     if (play.onclick)
-//     {
-//       break;
-//     }
-//     if (save.onclick)
-//     {
-//         bookmark = i;
-//     }
-//     i++;
-//   }
-// }
-
-// var fs = require('fs'),Ivona = require('../src/main');
-// ivona.listVoices().on('complete', function(voices) {
-//     console.log(voices);
-// });
+function Sound(source,volume,loop)
+{
+    this.source=source;
+    this.volume=volume;
+    this.loop=loop;
+    var son;
+    this.son=son;
+    this.finish=false;
+    this.stop=function()
+    {
+        document.body.removeChild(this.son);
+    }
+    this.start=function()
+    {
+        if(this.finish)return false;
+        this.son=document.createElement("embed");
+        this.son.setAttribute("src",this.source);
+        this.son.setAttribute("hidden","true");
+        this.son.setAttribute("volume",this.volume);
+        this.son.setAttribute("autostart","true");
+        this.son.setAttribute("loop",this.loop);
+        document.body.appendChild(this.son);
+    }
+    this.remove=function()
+    {
+        document.body.removeChild(this.son);
+        this.finish=true;
+    }
+    this.init=function(volume,loop)
+    {
+        this.finish=false;
+        this.volume=volume;
+        this.loop=loop;
+    }
+}
 
 var mainTextArray;
 var bookmark = 0;
@@ -82,33 +58,25 @@ $(document).ready(function(){
           $("#playButtonInText").attr("class", "button icon ion-stop");
       }
     });
+
     setInterval(function(){
+      // if ($("#playButtonInText").hasClass("ion-stop")) {
+      //   console.log("hello");
+      //   var foo = new Sound("../sound/VivaLaVida.mp3",80,true);
+      //   foo.start();
+      // }
+      //
       if ($("#playButtonInText").hasClass("ion-stop")) {
-        // var textFirstPart = "";
-        // var textSecondPart = "";
-        // for (var i = 0; i < bookmark; i++) {
-        //   textFirstPart = textFirstPart + sentenceTokens[i];
-        // }
-        // for (var i = bookmark+1; i < sentenceTokens.length; i++) {
-        //   textSecondPart = textSecondPart + sentenceTokens[i];
-        // }
-        // console.log(textFirstPart);
-        // console.log(textSecondPart);
-        // var result = textFirstPart + "<span style=\"background-color:yellow\">" + sentenceTokens[bookmark] + "</span>" + textSecondPart;
-        // $("#mainTextBook").html(result);
-        // bookmark = bookmark+1;
         var yourstring = sentenceTokens[bookmark];
-
-        $('p:contains('+yourstring+')', document.body).each(function(){
-          console.log(bookmark);
-          $(this).html($(this).html().replace(
-            new RegExp(yourstring, 'g'), '<span style=\"background-color: yellow\">'+ yourstring +'</span>'
-          ));
-        });
+        if (sentenceTokens[bookmark].length > 3) {
+          $('p:contains('+yourstring+')', document.body).each(function(){
+            console.log(bookmark);
+            $(this).html($(this).html().replace(
+              new RegExp(yourstring, 'g'), '<span style=\"background-color: #b5a0d6\">'+ yourstring +'</span>'
+            ));
+          });
+        }
+        bookmark = bookmark+1;
       }
-      //bookmark = bookmark+1;
     },2000);
-
 });
-//  [string] text - the text to be spoken
-//  [object] config (optional) - override Ivona request via 'body' value
